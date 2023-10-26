e1caeb193a44aea02c7e9929342af06f169e0cbf
Angel Alba Santos <analba-s@c3r3s1.42malaga.com>
2023-10-26 16:14:00 +0200

    GNL changes and printf opt

--- ft_printf/ft_putnbr_base.c
+++ ft_printf/ft_putnbr_base.c
@@ -6,102 +6,48 @@
 /*   By: analba-s <analba-s@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/25 22:22:15 by analba-sa         #+#    #+#             */
-/*   Updated: 2023/10/13 21:25:59 by analba-s         ###   ########.fr       */
+/*   Updated: 2023/10/18 20:20:46 by analba-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	get_length(char *base)
-{
-	int	length;
-
-	length = 0;
-	while (base[length])
-		length++;
-	return (length);
-}
-
-int	checking_base(char *base)
+static int	checker(char *b, int *len)
 {
 	int	i;
 
-	i = 0;
-	while (base[i])
+	i = -1;
+	while (b[*len])
+		*len += 1;
+	while (b[++i])
 	{
-		if (base[i] == '+' || base[i] == '-')
+		if ((b[i] == b[i + 1] || b[i] == '+' || b[i] == '-')
+			|| (b[i] < 32 || b[i] > 126))
 			return (0);
-		if (base[i] < 32 || base[i] > 126)
-			return (0);
-		i++;
 	}
 	return (1);
 }
 
-int	checker(char *base)
+void	ft_putnbr_base(long long nbr, char *base, int *i)
 {
-	int	i;
-	int	j;
+	int	len;
 
-	i = 0;
-	j = 0;
-	if (get_length(base) <= 1)
-		return (0);
-	if (!checking_base(base))
-		return (0);
-	while (base[i])
+	len = 0;
+	if (*i == -1)
+		return ;
+	if (checker(base, &len))
 	{
-		j = i + 1;
-		while (base[j])
-		{
-			if (base[i] == base[j])
-				return (0);
-			j++;
-		}
-		i++;
-	}
-	return (1);
-}
-
-void	nbr_esp(long nbr, char *base, int *ci)
-{
-	if (nbr < 0)
-	{
-		write (1, "-", 1);
-		nbr_esp(-nbr, base, ci);
-	}
-	else if (nbr < (get_length(base) + 1))
-	{
-		*ci += 1;
-		write (1, &base[nbr], 1);
-	}
-	else
-	{
-		nbr_esp(nbr / get_length(base), base, ci);
-		nbr_esp(nbr % get_length(base), base, ci);
-	}
-}
-
-void	ft_putnbr_base(int nbr, char *base, int *ci)
-{
-	int	i;
-
-	i = 0;
-	if (checker(base))
-	{
-		if (nbr == -2147483648)
-			nbr_esp((long)nbr, base, ci);
 		if (nbr < 0)
-			nbr_esp(nbr, base, ci);
-		else if (nbr < (get_length(base) + 1))
 		{
-			write (1, &base[nbr], 1);
-			*ci += 1;
+			ft_putchar_pf('-', i);
+			ft_putnbr_base(-nbr, base, i);
 		}
+		else if (nbr < len)
+			ft_putchar_pf(base[nbr], i);
 		else
 		{
-			ft_putnbr_base(nbr / get_length(base), base, ci);
-			ft_putnbr_base(nbr % get_length(base), base, ci);
+			ft_putnbr_base(nbr / len, base, i);
+			ft_putnbr_base(nbr % len, base, i);
 		}
 	}
 }
--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,13 +6,15 @@
 /*   By: analba-s <analba-s@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/05 15:40:36 by analba-sa         #+#    #+#             */
-/*   Updated: 2023/10/22 20:26:24 by analba-s         ###   ########.fr       */
+/*   Updated: 2023/10/26 15:35:27 by analba-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+
 # include "stdlib.h"
+# include "unistd.h"
 
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 1000
@@ -21,11 +23,12 @@
 char	*get_next_line(int fd);
 char	*ft_find_line(int fd, char *stcs);
 int		ft_strchr_gnl(char *s, int c);
-size_t	ft_strlen(char *s);
+int		ft_strlen(char *s);
 char	*ft_strjoin_gnl(char *stcs, char *buffer);
 char	*ft_get_line(char *stcs, int *i);
 char	*ft_new_stcs(char *stcs, int i);
-char	*ft_strdup(char *s1);
 char	*ft_free(char **str);
+char	*ft_calloc_gnl(int size);
+// void	ft_leaks(void);
 
 #endif
